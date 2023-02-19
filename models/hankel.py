dc45accc492ab8977f22293b820ae57e1987005a
Manu Shukla <manushukla1010@gmail.com>
2023-02-19 15:17:05 +0530

    All my code committed.We will ensure now to keep git updated.

---
+++ models/hankel.py
@@ -0,0 +1,16 @@
+import numpy as np
+class Hankel:
+    def __init__(self):
+        pass
+        # self.X = X
+        # self.lag = lag
+        # self.stride = int(stride_percent*lag)
+
+    def fit(self,X,lag,stride_percent):
+        stride = int(stride_percent*lag)
+        hankel = X[:lag].reshape(-1,1)
+        for i in range(stride,len(X),stride):
+            if i+lag < len(X):
+                new_col = X[i:i+lag].reshape(-1,1)
+                hankel = np.concatenate((hankel,new_col),axis=1)
+        return hankel
