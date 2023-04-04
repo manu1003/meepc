681d21379b8f945c4d70ce741df4083074da9e04
Gajender <gajenderstyle007@gmail.com>
2023-04-04 15:22:05 +0530

    update

--- models/Cluster.py
+++ models/Cluster.py
@@ -26,7 +26,7 @@ class Cluster:
                 silhouette_scores.append(score)
                 kmeans_objects.append(kmeans)
 
-        if silhouette_score:
+        if silhouette_scores:
             # Find the optimal number of clusters based on the highest silhouette score
             optimal_cluster_num = np.argmax(silhouette_scores) + 1 # Add 2 because the range starts at 2
             optimal_kmeans = kmeans_objects[optimal_cluster_num - 1] # Subtract 2 to get the index of the optimal number of clusters
