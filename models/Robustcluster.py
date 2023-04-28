626f84c6abe0a9bc4a66123200aef6e61c6802f9
Manu Shukla <manushukla1010@gmail.com>
2023-04-28 20:15:09 +0530

    ctown9 and Robustcluster

--- models/Robustcluster.py
+++ models/Robustcluster.py
@@ -1,37 +1,37 @@
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
+from copy import deepcopy
 import numpy as np
 
 class Robustcluster:
     def __init__(self) -> None:
         pass
 
-    import numpy as np
-
-    def kmeans(X, K, max_iters=100):
-        
-        
-        for i in range(max_iters):
-            
-            labels = np.argmin(distances, axis=0)
-            
-            # Update centroids to the mean of the assigned data points
-            for k in range(K):
-                centroids[k] = X[labels == k].mean(axis=0)
-        
-        return centroids, labels
-
-
-    def fit(self,X,optimal_k,alpha):
-
+    def fit(self,X,optimal_k,alpha_factor,tol=.001):
+        alpha = round(alpha_factor*len(X))
         # Initialize centroids randomly
         centroid_old = X[np.random.choice(range(len(X)), optimal_k)]
+        centroid_new = np.arange(optimal_k)
         converged = False
         while not converged:
             # Assign data points to the nearest centroid
+            distances = np.sqrt(((X - centroid_old[:,np.newaxis])**2))
+            idx = np.argsort(np.min(distances,axis=0))
+            labels = np.argmin(distances,axis=0)
+            labels_new = labels[idx[:-alpha]]
+            X_new = X[idx[:-alpha]]
             for i in range(optimal_k):
-                distances = np.sqrt(((X - centroid_old[i])**2).sum(axis=2))
+                centroid_new[i] = np.mean(X_new[labels_new==i])
 
-        np.any(centroid_new-centroid_old) > .001:
+            if np.all(centroid_new-centroid_old) <= tol:
+                converged = True
+            centroid_old = deepcopy(centroid_new)
+
+        return centroid_old,labels
+
+            
+            
+
+        
 
 
